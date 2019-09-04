ec640fbddb6824bedbac397f5a12693f91f0ff01
Sander <sanderdsz@gmail.com>
2019-09-03 23:13:08 -0300

    Add DB, User Model and Validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 // @route   POST api/users
-// @desc   Register user
+// @desc   Registra um usuario
 // @access Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Adicione um nome")
+      .not()
+      .isEmpty(),
+    check("email", "Por favor inclua um e-mail válido").isEmail(),
+    check("password", "Entre com um password de 6 ou mais caracteres").isLength(
+      { min: 6 }
+    )
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("Passou!");
+  }
+);
 
 module.exports = router;
