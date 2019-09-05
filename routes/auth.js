e586f67fea4289071aeebbdafd7e1ebb9d103291
Sander <sanderdsz@gmail.com>
2019-09-04 22:39:25 -0300

    Autenticated Routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 // @route   GET api/auth
 // @desc   Recebe usuario logado
@@ -11,8 +17,59 @@ router.get("/", (req, res) => {
 // @route   POST api/auth
 // @desc   Autentica usuario & recebe token
 // @access Publico
-router.post("/", (req, res) => {
-  res.send("Loga um usuario");
-});
+// Validação para ver email
+router.post(
+  "/",
+  [
+    check("email", "Por favor insira um email válido").isEmail(),
+    check("password", "Insira um password").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      // Ver se usuário já existe
+      let user = await User.findOne({ email });
+      // Se não há usuário retorna erro de credenciais
+      if (!user) {
+        return res.status(400).json({ msg: "Credenciais inválidas" });
+      }
+      // Se há usuário continua verificando password
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      // True ou False se o password da math com hash
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Credenciais inválidas" });
+      }
+
+      // Envia token se está certo
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          // Tempo para expirar sessão
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Erro no servidor");
+    }
+  }
+);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
@@ -43,7 +45,25 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      res.send("Usuário registrado");
+      //JWT
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          // Tempo para expirar sessão
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Erro no servidor");
