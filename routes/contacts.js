4cb0db75670899aa0b6d0bab96d1e634e9896656
Sander <sanderdsz@gmail.com>
2019-09-05 12:51:58 -0300

    Contact model and Contacts route added

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 // @route   GET api/contacts
 // @desc   Recebe todos contatos do usuario
 // @access Private
-router.get("/", (req, res) => {
-  res.send("Recebe todos contatos");
+router.get("/", auth, async (req, res) => {
+  // Try-catch necessário para mongoose
+  try {
+    /*
+     * Recebe contato do usuário especifico => req.user.id
+     * Sort por datas => -1 significa o contato mais recente
+     */
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.menssage);
+    res.status(500).send("Erro no servidor");
+  }
 });
 
 // @route   POST api/contacts
