003f323212c8ab5fdfecbfa0f6853f521e25b699
Sander <sanderdsz@gmail.com>
2019-09-05 11:28:50 -0300

    Files cleanup and App styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import logo from "./logo.svg";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
@@ -21,17 +20,12 @@ function App() {
   return (
     <Router>
       <Fragment>
-        <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
         </Switch>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <code>Hello world!</code>
-          </p>
-        </header>
+        <NavBar />
+        <div className="App-background" />
       </Fragment>
     </Router>
   );
